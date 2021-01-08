02f57a03e43f727d9bc3f1e29f85e82be0dd5599
JHLeeeMe-T420 <lejung92@gmail.com>
2021-01-08 12:12:20 +0900

    [Modify] typo
    	수정함:        java/creational/BuilderPattern.java

--- java/creational/BuilderPattern.java
+++ java/creational/BuilderPattern.java
@@ -152,13 +152,13 @@ class MealBuilder {
 
         return meal;
     }
+
     public Meal prepareNonVegMeal() {
         Meal meal = new Meal();
         meal.addItem(new ChickenBurger());
         meal.addItem(new Pepsi());
 
         return meal;
-
     }
 }
 
