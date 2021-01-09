e28f89e9f9585ea70d6aba48764c37bd0a5e6760
JHLeeeMe-T420 <lejung92@gmail.com>
2021-01-10 04:07:50 +0900

    [Add] Facade Pattern
    	새 파일:       java/structural/FacadePattern.java

---
+++ java/structural/FacadePattern.java
@@ -0,0 +1,60 @@
+/**
+ * Facade Pattern
+ *   복잡한 lib들을 Facade 클래스로 간단하게 사용가능한 API 제공 
+ */
+
+class CPU {
+    public void freeze() { 
+        System.out.println("CPU::freeze()\n"); 
+    }
+
+    public void jump(long bootAddress) { 
+        System.out.println("CPU::jump(bootAddress)");
+        System.out.println("addr: " + bootAddress + "\n"); 
+    }
+
+    public void execute() { 
+        System.out.println("CPU::excute()"); 
+    }
+}
+
+class Memory {
+    public void load(long bootAddress, byte[] data) { 
+        System.out.println("Memory::load(bootAddress, data)");
+        System.out.println("addr: " + bootAddress + "\n");
+    }
+}
+
+class HardDrive {
+    public byte[] read(long bootSector, int sectorSize) {
+        System.out.println("HardDrive::read(bootSector, sectorSize)");
+        System.out.println("sector: " + bootSector + ", size: " + sectorSize + "\n");
+
+        return new byte[] {};
+    }
+}
+
+/* Façade */
+class Computer {
+    final long BOOT_ADDRESS = 102L;
+    final long BOOT_SECTOR = 204L;
+    final int SECTOR_SIZE = 1000;
+
+    public void startComputer() {
+        CPU cpu = new CPU();
+        Memory memory = new Memory();
+        HardDrive hardDrive = new HardDrive();
+
+        cpu.freeze();
+        memory.load(BOOT_ADDRESS, hardDrive.read(BOOT_SECTOR, SECTOR_SIZE));
+        cpu.jump(BOOT_ADDRESS);
+        cpu.execute();
+    }
+}
+
+public class FacadePattern {
+    public static void main(String[] args) {
+        Computer computer = new Computer();
+        computer.startComputer();
+    }
+}
